8be0c90c5eadc090f666e589925854f57bc967f3
sovanden <sovanden@CL9515.psestaff.local>
2025-03-14 10:43:54 +0700

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/denpse/next-cents.git",
+  base: "/next-cents",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
